7ca02a64f60da067e784b61ddb7689ab2212d1b8
dfili8 <dfilimon.s@gmail.com>
2018-08-13 20:13:45 -0400

    New features

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const opponentName = 'Grant the Almighty Chicken';
-let userName;
-let userHealthPoints = 40;
-let opponentHealthPoints = 10;
-let userWins = 0;
-
-const question = prompt('Do you want to play?');
-const answer = question.toLowerCase();
-
-if(answer === 'yes'){
-    startGame();
-} else {
-    console.log('Try again!');
-}
-
-function startGame(){
-    userName = prompt('Enter your name:');
-    fightToTheDeath();
-}
-
-function fightToTheDeath() {
-    while(userWins < 3 && userHealthPoints > 0) {
-        userHealthPoints -= Math.floor(Math.random() * 2) + 1;
-        opponentHealthPoints -= Math.floor(Math.random() * 2) + 1;
-        console.log(`${userName} has ${userHealthPoints} health points left`);
-        console.log(`${opponentName} has ${opponentHealthPoints} health points left`);
-        if(opponentHealthPoints < 1){
-            userWins++;
-            console.log(`${userName} has won this round.`);
-            opponentHealthPoints = 10;
-        } 
-
-        if (userWins === 3) {
-            console.log(`Game Over. ${userName} is victorious!`);
-        } else if (userHealthPoints < 1) {
-            console.log(`Game Over. ${opponentName} is victorious!`);
+"use strict"; {
+    const opponentName = 'Grant the Almighty Chicken';
+    let userName;
+    let userHealthPoints = 40;
+    let opponentHealthPoints = 10;
+    let userWins = 0;
+  
+    function startGame(){
+        const question = prompt('Do you want to play?');
+        const answer = question.toLowerCase();
+            if(answer === 'yes'){
+                userName = prompt('Enter your name:');
+                startCombat();
+            } else {
+                console.log('Try again!');
+            }
+    }
+  
+    function getDamage(){
+        userHealthPoints -= Math.floor(Math.random() * 5) + 1;
+        opponentHealthPoints -= Math.floor(Math.random() * 5) + 1;
+    }
+  
+    function startCombat(){
+        while(userWins < 3 && userHealthPoints > 0) {
+            const action = prompt('Would you like to "attack" or "quit"?');
+            if (action === 'attack'){
+                getDamage();
+                console.log(`${userName} has ${userHealthPoints} health points left`);
+                console.log(`${opponentName} has ${opponentHealthPoints} health points left`);
+                if(opponentHealthPoints < 1){
+                    userWins++;
+                    console.log(`${userName} has won this round.`);
+                    opponentHealthPoints = 10;
+                } 
+            
+                if(userWins === 3) {
+                    console.log(`Game Over. ${userName} is victorious!`);
+                } else if (userHealthPoints < 1) {
+                    console.log(`Game Over. ${opponentName} is victorious!`);
+                }
+            } else {
+                break;
+            }
         }
     }
+    startGame();
 }
